903d9f068232e2cf739e261443c5e1723cdfcb2d
hemant_revankar <hemant_revankar@persistent.com>
2018-07-13 17:28:28 +0530

    Included necessary application setup with some test class to confirm

---
+++ src/main/java/com/springboot/exercise/dictionary/test/TestController.java
@@ -0,0 +1,25 @@
+package com.springboot.exercise.dictionary.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	private static AtomicInteger counter = new AtomicInteger(1);
+	
+	@RequestMapping(value="/greet/{name}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Greeting greet(@PathVariable("name") String name) {
+		return new Greeting(counter.getAndIncrement(), "Hello " + name);
+	}
+	
+	@RequestMapping(value="/greet", method=RequestMethod.GET)
+	public String greet() {
+		return "Hello User..!";
+	}
+}
